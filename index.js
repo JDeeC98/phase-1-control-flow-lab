5dfbf35925e0b570a32125930de8e7223115813d
John David Carroll <johndavidcarroll@gmail.com>
2021-11-09 15:22:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+if (someValue <= 400)
+{return "This one is on me!"}
+else if (someValue <= 2499)
+{return "I will gladly take your thirty bucks."}
+else (someValue >= 2500)
+{return "No can do."}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+  return ternaryCheckCity = location === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case "generous" :
+    return "Thank you so much.";
+  case "not as generous" :
+    return "Thank you.";
+  default: return "Bye.";
+} 
 }
